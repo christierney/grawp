88167d013c5ec0460478612201c0a1985beb0994
Chris Tierney <chris.tierney@gmail.com>
2012-03-28 17:21:02 -0400

    Add some more interesting json examples.

--- src/main/groovy/GrawpSerializer.groovy
+++ src/main/groovy/GrawpSerializer.groovy
@@ -25,7 +25,12 @@ class GrawpSerializer implements MessageBodyWriter<Grawp> {
     void writeTo(Grawp t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) {
         entityStream.withWriter { w ->
             def json = new StreamingJsonBuilder(w)
-            json name : t.name
+            json {
+                    "thename" t.name // json names don't have to match bean property names
+                    "age" t.age
+                    "@rbitrary" true // you're not limited to properties of the bean
+                    "siblings"  t.siblings // collection is automatically turned into a json array
+            }
         }
     }
 }
--- src/main/java/Grawp.java
+++ src/main/java/Grawp.java
@@ -1,12 +1,31 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Grawp {
     private final String name;
+    private final int age;
+    private Set<Grawp> siblings;
 
-    public Grawp(String name) {
+    public Grawp(String name, int age) {
         this.name = name;
+        this.age = age;
+        this.siblings = new HashSet<Grawp>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Set<Grawp> getSiblings() {
+        return Collections.unmodifiableSet(siblings);
+    }
+    
+    public void addSibling(Grawp sibling) {
+        siblings.add(sibling);
     }
 }
